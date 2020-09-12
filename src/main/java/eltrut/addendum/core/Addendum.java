7859b54103f7347b25f357ddd00cfe53ba126cd0
EltrutLikes <10eltrut1@gmail.com>
2020-09-11 22:09:52 -0400

    yayyy new mod plz don't crash

---
+++ src/main/java/eltrut/addendum/core/Addendum.java
@@ -0,0 +1,45 @@
+package eltrut.addendum.core;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.teamabnormals.abnormals_core.core.utils.RegistryHelper;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod("addendum")
+@Mod.EventBusSubscriber(modid = "addendum", bus = Bus.MOD)
+public class Addendum
+{
+    private static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "addendum";
+    public static final RegistryHelper REGISTRY_HELPER = new RegistryHelper(MOD_ID);
+    public static Addendum instance;
+
+    IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+    public Addendum() {
+        modEventBus.addListener(this::doCommonStuff);
+        modEventBus.addListener(this::doClientStuff);
+        instance = this;
+        
+        REGISTRY_HELPER.getDeferredItemRegister().register(modEventBus);
+        REGISTRY_HELPER.getDeferredBlockRegister().register(modEventBus);
+        
+        MinecraftForge.EVENT_BUS.register(this);
+        
+    }
+
+    private void doCommonStuff(final FMLCommonSetupEvent event)
+    {
+    }
+    
+    private void doClientStuff(final FMLClientSetupEvent event) {
+    }
+}
---
+++ src/main/java/eltrut/addendum/core/BlockInit.java
@@ -0,0 +1,12 @@
+package eltrut.addendum.core;
+
+import com.teamabnormals.abnormals_core.core.utils.RegistryHelper;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+
+@Mod.EventBusSubscriber(modid = Addendum.MOD_ID, bus = Bus.MOD)
+public class BlockInit {
+	public static final RegistryHelper HELPER = Addendum.REGISTRY_HELPER;
+	
+}
